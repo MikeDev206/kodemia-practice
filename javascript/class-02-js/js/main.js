9e5d5a60a3eb7e0194c0efd99518b7d361e912ad
Miguel Angel Aceves <acevesm77@gmail.com>
2020-03-11 12:21:24 -0600

    2nd chllng PEN to USD & EUR

--- javascript/class-02-js/js/main.js
+++ javascript/class-02-js/js/main.js
@@ -5,3 +5,13 @@ var age = 25;
 var days = 365;
 
 console.log(age * days);
+
+// 2nd challenge
+// Tengo 2500 soles, ¿Cuántos dólares y cuantos euros tengo?
+
+var PEN = 2500;
+var USD = 0.28;
+var EUR = 0.25;
+
+console.log(`You owe${USD * PEN}dollars`);
+console.log(`&${EUR * PEN}euros.`);
