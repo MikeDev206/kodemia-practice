64811dff1eb3f396407fc4f177e17ae5a99ff7b6
Miguel Angel Aceves <acevesm77@gmail.com>
2020-03-11 11:55:21 -0600

    last commit from intro

--- javascript/myAwesomeProject/js/main.js
+++ javascript/myAwesomeProject/js/main.js
@@ -3,4 +3,4 @@ console.log('Desde el src prro')
 var firstNumer = 10;
 var secondNumber = 20;
 
-((secondNumber % firstNumer) + (3 * firstNumer / 2) + 10)
+((secondNumber % firstNumer) + (3 * firstNumer / 2) + 10);
