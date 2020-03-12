847d4f0359fcae436947bb10c05c20af682cb9e3
Miguel Angel Aceves <acevesm77@gmail.com>
2020-03-12 17:28:49 -0600

    first project w react-native

---
+++ javascript/class-03-js/js/main.js
@@ -0,0 +1,12 @@
+//1st chllng
+//Dada una palabra imprimir en pantalla si la palabra tiene menos de 8 letras
+
+var word = "prro";
+
+word.length
+
+if (word.length === 8) {
+    console.log("Indeed it has 8 letters");
+} else {
+    console.log("Nope, try again pls");
+}
